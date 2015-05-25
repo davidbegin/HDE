c0cd2b22147b818f2e33323c0da2fb1efeb0f14f
David Begin <davidmichaelbe@gmail.com>
2015-05-25 16:31:14 -0700

    Some unnusal test setup, but almost to something useful
      first need to learn how to query

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,17 @@ extern crate type_printer;
 //         name:
 
 // TODO:
-//     1. add uniqueness constraint around Company names
 //     2. finish creating basic schema
 //          a. associating the tables
 //          b. building some basic queries
 //
-//     3. turn everything into methods
 //     4. add some functions for cleaning state
 //     5. get some benchmarks
 //     6. gets some tests
 //     7. figure how to dump the schema and data
+//     8. seperate out into modules
+//     9. find out to show better errors from postgres
+//     10. learn more about what postgres offers
 
 fn main() {
     title();
@@ -38,22 +39,30 @@ fn main() {
 
     database_creator::create_companies_table(&conn);
     database_cleaner::clear_companies(&conn);
-
-    let stmt = match conn.prepare("insert into companies (name) values ($1)") {
-        Ok(stmt) => stmt,
-        Err(e) => {
-            println!("having touble preparing to insert company");
-            return;
-        }
-    };
-
-    for company in companies::all() {
-        stmt.execute(&[&company])
-            .ok()
-            .expect("there was a problem inserting company");
-    }
-
+    database_seeder::seed_companies(&conn);
     database_dumper::companies(&conn);
+    database_cleaner::drop_companies_table(&conn);
+}
+
+mod database_seeder {
+    use companies;
+    use postgres::Connection;
+
+    pub fn seed_companies(conn: &Connection) {
+        let stmt = match conn.prepare("insert into companies (name) values ($1)") {
+            Ok(stmt) => stmt,
+            Err(e) => {
+                println!("having touble preparing to insert company");
+                return;
+            }
+        };
+
+        for company in companies::all() {
+            stmt.execute(&[&company])
+                .ok()
+                .expect("there was a problem inserting company");
+        }
+    }
 }
 
 mod companies {
@@ -84,8 +93,8 @@ mod database_creator {
     pub fn create_companies_table(conn: &Connection) {
         conn.execute("
             CREATE TABLE IF NOT EXISTS companies (
-                id serial primary key,
-                name text
+                id SERIAL PRIMARY KEY,
+                name TEXT UNIQUE
             )",
         &[]).ok().expect("could not create companies table");
     }
@@ -96,6 +105,8 @@ mod database_dumper {
 
     // TODO: make this have a timestamp
     // make the path configurable
+    //
+    // find out other options other than CSV (try NULL and DELIMTER)
     pub fn companies(conn: &Connection) {
         conn.execute("Copy (Select * From companies) To '/Users/dbegin/rust/hde/dump/companies.csv' With CSV", &[])
             .ok()
@@ -109,6 +120,11 @@ mod database_cleaner {
     pub fn clear_companies(conn: &Connection) {
         println!("Deleting all companies...");
         conn.execute("DELETE FROM companies", &[]).ok().expect("could not delete companies");
+    }
+
+    pub fn drop_companies_table(conn: &Connection) {
+        println!("Dropping the companies table...");
+        conn.execute("DROP TABLE IF EXISTS companies", &[]).ok().expect("could not drop table companies");
     }
 }
 
@@ -134,3 +150,49 @@ mod config {
 fn title() {
     println!("\nA Project Combing 3 of my interests, Rust, Postgres and Watches\n");
 }
+
+#[cfg(test)]
+mod tests {
+    use config;
+    use database_creator;
+    use postgres::Connection;
+
+    #[test]
+    fn it_can_be_tested() {
+        assert_eq!(true, true);
+    }
+
+    #[test]
+    fn it_can_find_all_companies() {
+        let connection_option = config::database_connection();
+
+        let conn = match connection_option {
+            Some(conn) => conn,
+            None => {
+                println!("Looks like we got a nil connection");
+                return;
+            }
+        };
+
+        before_each(&conn);
+
+        let result = conn.execute("SELECT * FROM companies", &[])
+            .ok()
+            .expect("there was a problem querying");
+
+        assert_eq!(true, true);
+
+
+        // let stmt = try!(conn.prepare("SELECT bar, baz FROM foo"));
+        // for row in try!(stmt.query(&[])) {
+        //     let bar: i32 = row.get(0);
+        //     let baz: String = row.get("baz");
+        //     println!("bar: {}, baz: {}", bar, baz);
+        // }
+    }
+
+    fn before_each(conn: &Connection) {
+        database_creator::create_companies_table(&conn);
+    }
+}
+
