b28160ec156cc5348cdd1ad143aff8ba3eec8773
David Begin <davidmichaelbe@gmail.com>
2015-05-24 19:42:53 -0700

    I have a single piece of data in the DB!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+#![allow(unused_imports, unused_variables)]
+
+extern crate postgres;
+
+use postgres::{Connection, SslMode};
+
 fn main() {
-    println!("A Project Combing 3 of my interests, Rust, Postgres and Watches");
+    println!("\nA Project Combing 3 of my interests, Rust, Postgres and Watches\n");
+
+    let database = "postgresql://dbegin@localhost/watches";
+
+    let conn = match Connection::connect(database, &SslMode::None) {
+        Ok(conn) => conn,
+        Err(e) => {
+            println!("connection error: {:?}", e);
+            return;
+        }
+    };
+
+    // Lets start with something like this:
+    // Company
+    //     name:
+    // Watch
+    //     series:
+    //     ref:
+    // Movement
+    //     name:
+
+    conn.execute("
+        CREATE TABLE IF NOT EXISTS companies (
+            id serial primary key,
+            name text
+        )
+    ", &[]).ok().expect("could not create companies table");
+
+    let stmt = match conn.prepare("insert into companies (name) values ($1)") {
+        Ok(stmt) => stmt,
+        Err(e) => {
+            println!("having touble preparing to insert company");
+            return;
+        }
+    };
+
+    let first_company = format!("Panerai");
+    stmt.execute(&[&first_company]).ok().expect("Dang it!");
 }
