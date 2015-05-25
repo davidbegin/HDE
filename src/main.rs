5489167818c1728b4931d9139f867cf423f42f6c
David Begin <davidmichaelbe@gmail.com>
2015-05-25 15:21:33 -0700

    breaking down the code into mods and fns

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
-#![allow(unused_imports, unused_variables)]
+#![allow(unused_imports, unused_variables, dead_code)]
 
 extern crate postgres;
+extern crate type_printer;
 
-use postgres::{Connection, SslMode};
+// MVP SCHEMA:
+//     Company
+//         name:
+//     Watch
+//         series:
+//         ref:
+//     Movement
+//         name:
+
+// TODO:
+//     1. add uniqueness constraint around Company names
+//     2. finish creating basic schema
+//          a. associating the tables
+//          b. building some basic queries
+//
+//     3. turn everything into methods
+//     4. add some functions for cleaning state
+//     5. get some benchmarks
+//     6. gets some tests
 
 fn main() {
-    println!("\nA Project Combing 3 of my interests, Rust, Postgres and Watches\n");
+    title();
+    let connection_option = config::database_connection();
 
-    let database = "postgresql://dbegin@localhost/watches";
-
-    let conn = match Connection::connect(database, &SslMode::None) {
-        Ok(conn) => conn,
-        Err(e) => {
-            println!("connection error: {:?}", e);
+    let conn = match connection_option {
+        Some(conn) => conn,
+        None => {
+            println!("Looks like we got a nil connection");
             return;
         }
     };
-
-    // Lets start with something like this:
-    // Company
-    //     name:
-    // Watch
-    //     series:
-    //     ref:
-    // Movement
-    //     name:
 
     conn.execute("
         CREATE TABLE IF NOT EXISTS companies (
@@ -60,4 +69,29 @@ fn main() {
             .ok()
             .expect("there was a problem inserting company");
     }
+}
+
+mod config {
+    use postgres::{Connection, SslMode};
+
+    pub fn database_connection() -> Option<Connection> {
+        let connection_option = match Connection::connect(database_url(), &SslMode::None) {
+            Ok(connection_option) => Some(connection_option),
+            Err(e) => {
+                println!("connection error: {:?}", e);
+                None
+            }
+        };
+
+        connection_option
+    }
+
+    fn database_url() -> &'static str {
+        let database_url: &'static str = "postgresql://dbegin@localhost/watches";
+        database_url
+    }
+}
+
+fn title() {
+    println!("\nA Project Combing 3 of my interests, Rust, Postgres and Watches\n");
 }
