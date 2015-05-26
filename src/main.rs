7b30bca650d81d76d294e3bd6c99855c5b2ef4eb
David Begin <davidmichaelbe@gmail.com>
2015-05-25 18:12:30 -0700

    a function that returns results form a database!

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ extern crate type_printer;
 //
 //     4. add some functions for cleaning state
 //     5. get some benchmarks
-//     6. gets some tests
 //     7. figure how to dump the schema and data
 //     8. seperate out into modules
 //     9. find out to show better errors from postgres
@@ -38,18 +37,37 @@ fn main() {
     };
 
     // database_creator::create_companies_table(&conn);
+    // database_cleaner::clear_companies(&conn);
     // database_seeder::seed_companies(&conn);
     // database_dumper::companies(&conn);
-    // database_cleaner::clear_companies(&conn);
     // database_cleaner::drop_companies_table(&conn);
-    database_querier::select_all_companies(&conn);
+    let names = database_querier::select_all_company_names(&conn);
+
+    println!("Names: {:?}", names);
 }
 
 mod database_querier {
     use postgres::Connection;
     use type_printer;
 
-    pub fn select_all_companies(conn: &Connection) {
+    pub fn select_all_company_names(conn: &Connection) -> Vec<String> {
+        let stmt = match conn.prepare("SELECT * FROM companies") {
+            Ok(stmt) => stmt,
+            Err(e) => {
+                return vec![];
+            }
+        };
+
+        let result = stmt.query(&[]).ok().expect("dang it");
+        let mapped_result = result.iter().map(|i| {
+            let name: String = i.get("name");
+            name
+        }).collect::<Vec<String>>();
+
+        mapped_result
+    }
+
+    pub fn print_all_companies(conn: &Connection) {
         let stmt = match conn.prepare("SELECT * FROM companies") {
             Ok(stmt) => stmt,
             Err(e) => {
@@ -59,12 +77,16 @@ mod database_querier {
 
         let result = stmt.query(&[]).ok().expect("dang it");
 
-        for row in result {
-            let name: String = row.get("name");
-            println!("name: {:?}", name);
-        }
+        println!("\n\tCompanies:\n");
 
-        println!("all companies");
+        // TODO: I need to handle the fancy characters being read from the DB
+        for row in result {
+            let id: i32 = row.get("id");
+            let name: String = row.get("name");
+            println!("id: {:?}", id);
+            println!("name: {:?}", name);
+            println!("-------------------------------------");
+        }
     }
 }
 
@@ -95,7 +117,7 @@ mod companies {
         // and then I want to experiment with dumping the data
         let first_company = format!("Panerai");
         let second_company = format!("Rolex");
-        let third_company = format!("A. Lange & Söhne");
+        let third_company = format!("A. Lange & Sohne");
         let fourth_company = format!("Audemars Piguet");
         let fifth_company = format!("IWC Schaffhausen");
 
