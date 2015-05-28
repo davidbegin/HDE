6ce5a191b1891df0197474f851b18392511d8a75
David Begin <davidmichaelbe@gmail.com>
2015-05-27 21:46:30 -0700

    woah, the type checker just helped me correct CSV
      ...very quickly, that ruled!

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 extern crate postgres;
 extern crate type_printer;
+extern crate csv;
 mod database_creator;
 mod database_seeder;
 mod database_cleaner;
@@ -24,30 +25,41 @@ fn main() {
     // database_cleaner::clear_watches(&conn);
     // database_cleaner::clear_movements(&conn);
 
-    database_cleaner::drop_companies_table(&conn);
-    database_cleaner::drop_watches_table(&conn);
-    database_cleaner::drop_movements_table(&conn);
-
-    database_creator::create_companies_table(&conn);
-    database_creator::create_movements_table(&conn);
-    database_creator::create_watches_table(&conn);
-    database_creator::add_company_id_to_watches(&conn);
-
-    database_seeder::seed_companies(&conn);
-    database_seeder::seed_movements(&conn);
-    database_seeder::seed_watches(&conn);
-    database_seeder:: associate_movements_and_watches(&conn);
+    // database_cleaner::drop_companies_table(&conn);
+    // database_cleaner::drop_watches_table(&conn);
+    // database_cleaner::drop_movements_table(&conn);
+    //
+    // database_creator::create_companies_table(&conn);
+    // database_creator::create_movements_table(&conn);
+    // database_creator::create_watches_table(&conn);
+    // database_creator::add_company_id_to_watches(&conn);
+    //
+    // database_seeder::seed_companies(&conn);
+    // database_seeder::seed_movements(&conn);
+    // database_seeder::seed_watches(&conn);
+    // database_seeder:: associate_movements_and_watches(&conn);
 
     // database_dumper::companies(&conn);
     // database_dumper::watches(&conn);
     // database_dumper::movements(&conn);
 
-    database_querier::print_all_companies(&conn);
-    database_querier::print_all_watches(&conn);
-    database_querier::print_all_movements(&conn);
+    // database_querier::print_all_companies(&conn);
+    // database_querier::print_all_watches(&conn);
+    // database_querier::print_all_movements(&conn);
+
+    csv_experiments();
 }
 
 fn title() {
     println!("\nA Project Combing 3 of my interests, Rust, Postgres and Watches\n");
 }
 
+fn csv_experiments() {
+    println!("Time to parse some CSV");
+
+    let mut reader = csv::Reader::from_file("./src/database_seeder/data/test_watch_data_1.csv").unwrap();
+
+    for record in reader.decode() {
+        let (s1, s2, dist): (String, String, usize) = record.unwrap();
+    }
+}
