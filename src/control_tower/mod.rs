915adb1518fbd7af518ccbc1c2ca5b07a03ad1ed
David Begin <davidmichaelbe@gmail.com>
2015-05-27 23:15:51 -0700

    ground control to major tom

---
+++ src/control_tower/mod.rs
@@ -0,0 +1,53 @@
+use postgres::Connection;
+
+// Is this cool?
+use {
+    database_cleaner,
+    database_creator,
+    database_seeder,
+    database_dumper,
+    database_querier
+};
+
+pub fn title() {
+    println!("\nA Project Combing 3 of my interests, Rust, Postgres and Watches\n");
+}
+
+pub fn clear(conn: &Connection) {
+    database_cleaner::clear_companies(&conn);
+    database_cleaner::clear_watches(&conn);
+    database_cleaner::clear_movements(&conn);
+}
+
+pub fn drop_tables(conn: &Connection) {
+    database_cleaner::drop_companies_table(&conn);
+    database_cleaner::drop_watches_table(&conn);
+    database_cleaner::drop_movements_table(&conn);
+}
+
+pub fn create_tables(conn: &Connection) {
+    database_creator::create_companies_table(&conn);
+    database_creator::create_movements_table(&conn);
+    database_creator::create_watches_table(&conn);
+    database_creator::add_company_id_to_watches(&conn);
+}
+
+pub fn seed_database(conn: &Connection) {
+    database_seeder::seed_companies(&conn);
+    database_seeder::seed_movements(&conn);
+    database_seeder::seed_watches(&conn);
+    database_seeder::associate_movements_and_watches(&conn);
+    database_seeder::seed_from_csv(&conn);
+}
+
+pub fn copy_database(conn: &Connection) {
+    database_dumper::companies(&conn);
+    database_dumper::watches(&conn);
+    database_dumper::movements(&conn);
+}
+
+pub fn blue_angels(conn: &Connection) {
+    database_querier::print_all_companies(&conn);
+    database_querier::print_all_watches(&conn);
+    database_querier::print_all_movements(&conn);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,19 @@
 extern crate postgres;
 extern crate type_printer;
 extern crate csv;
+
 mod database_creator;
-mod database_seeder;
 mod database_cleaner;
 mod database_dumper;
 mod database_querier;
+mod database_seeder;
 mod config;
 mod tests;
+mod control_tower;
 
 fn main() {
-    title();
+    control_tower::title();
+
     let conn = match config::database_connection() {
         Some(conn) => conn,
         None => {
@@ -21,34 +24,10 @@ fn main() {
         }
     };
 
-    // database_cleaner::clear_companies(&conn);
-    // database_cleaner::clear_watches(&conn);
-    // database_cleaner::clear_movements(&conn);
-    //
-    // database_cleaner::drop_companies_table(&conn);
-    // database_cleaner::drop_watches_table(&conn);
-    // database_cleaner::drop_movements_table(&conn);
-
-    // database_creator::create_companies_table(&conn);
-    // database_creator::create_movements_table(&conn);
-    // database_creator::create_watches_table(&conn);
-    // database_creator::add_company_id_to_watches(&conn);
-    //
-    // database_seeder::seed_companies(&conn);
-    // database_seeder::seed_movements(&conn);
-    // database_seeder::seed_watches(&conn);
-    // database_seeder::associate_movements_and_watches(&conn);
-    // database_seeder::seed_from_csv(&conn);
-
-    // database_dumper::companies(&conn);
-    // database_dumper::watches(&conn);
-    // database_dumper::movements(&conn);
-
-    database_querier::print_all_companies(&conn);
-    database_querier::print_all_watches(&conn);
-    database_querier::print_all_movements(&conn);
-}
-
-fn title() {
-    println!("\nA Project Combing 3 of my interests, Rust, Postgres and Watches\n");
+    control_tower::clear(&conn);
+    control_tower::drop_tables(&conn);
+    control_tower::create_tables(&conn);
+    control_tower::seed_database(&conn);
+    control_tower::copy_database(&conn);
+    control_tower::blue_angels(&conn);
 }
