5b80ba9413027c95d8481b84b7c824f91e8aa1c5
David Begin <davidmichaelbe@gmail.com>
2015-06-02 21:59:27 -0700

    Jah Bless Postgres and Returning
      and rust for just working, by changing my execute to a query

--- src/main.rs
+++ src/main.rs
@@ -48,8 +48,8 @@ fn main() {
 
 fn time_to_try_to_summon_the_ghost_of_oo() {
     let watch1 = Watch::new("Millguass".to_owned());
-    println!("My first Watch struct: {:?}", watch1);
-    Watch::klass();
+    let id: i32 = watch1.save();
+    println!("New Watch: {} : {}", id, watch1);
 }
 
 struct Watch {
@@ -57,25 +57,41 @@ struct Watch {
 }
 
 impl Watch {
-    fn klass() {
-        println!("class: Watch");
-    }
-
      fn new(name: String) -> Watch{
          Watch {
              name: name
          }
      }
+
+     fn save(&self) -> i32 {
+         let conn = config::database_connection().unwrap();
+
+         let stmt = conn.prepare("INSERT INTO watches (name) VALUES ($1) RETURNING id")
+             .ok()
+             .expect("could not prepare to insert into watches");
+
+         let mut rows = stmt.query(&[&self.name]).ok().expect("cold not query");
+
+         let row = rows.iter().next().unwrap();
+         let id: i32 = row.get("id");
+         id
+     }
 }
 
 impl fmt::Debug for Watch{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Watch: {}", self.name)
+        write!(f, "{}", self.name)
+    }
+}
+
+impl fmt::Display for Watch{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)
     }
 }
 
 impl Drop for Watch {
     fn drop(&mut self) {
-        println!("Watch {} deallocated", self.name);
+        // println!("Watch {} deallocated", self);
     }
 }
